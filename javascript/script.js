759f00d0df24cb33bbff06cd3ff090e94b5e8ced
nogami777 <78405789+nogami777@users.noreply.github.com>
2021-02-25 18:09:45 +0900

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -40,8 +40,8 @@ $(function() {
 });
 
 $(function(){
-	$('.js-modal-open').on('click',function(){
-		$('.js-modal').fadeIn();
+	$('js-modal-open').on('click',function(){
+		$('js-modal').fadeIn();
 		return false;
 	});
 	$('.js-modal-close').on('click',function(){
