153b75770a8e3e17b6a5736b2f08f152e3b15b7f
nogami777 <78405789+nogami777@users.noreply.github.com>
2021-03-02 14:55:42 +0900

    Update script.js

--- Introduction/javascript/script.js
+++ Introduction/javascript/script.js
@@ -77,8 +77,8 @@ $(function() {
 
 $(function() {
 	$('.js-modal-open').on('click',function(){
-		var position = target.offset().top; //ヘッダの高さ分位置をずらす
 		$('.js-modal').fadeIn();
+		var position = target.offset().top; //ヘッダの高さ分位置をずらす
 		return false;
 	});
 	$('.js-modal-close').on('click',function(){
