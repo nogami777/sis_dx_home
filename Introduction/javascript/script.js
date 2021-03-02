6cd64e537018c23c2679c7f0c90433d1ed0c2077
nogami777 <78405789+nogami777@users.noreply.github.com>
2021-03-02 19:23:25 +0900

    Update script.js

--- Introduction/javascript/script.js
+++ Introduction/javascript/script.js
@@ -77,17 +77,8 @@ $(function() {
 
 $(function() {
 	$('.js-modal-open').on('click',function(){
-		
 		var position = $(this).offset().top; //ヘッダの高さ分位置をずらす
-		var img = new Array (
-			'images/omikuji_daikichi.png',
-			'images/omikuji_chuukichi.png',
-			'images/omikuji_syoukichi.png',
-			'images/omikuji_kyou.png',
-		);
-		var random = Math.floor(Math.random() * img.length);
-		var result = '<img src =' +img[random]+ '>';
-		document.getElementById('result').innerHTML = result;
+		omikuji();
 		$('.js-modal').fadeIn();
 		return false;
 	});
@@ -96,3 +87,16 @@ $(function() {
 		return false;
 	});
 });
+
+function omikuji() {
+	var img = new Array (
+			'images/omikuji_daikichi.png',
+			'images/omikuji_chuukichi.png',
+			'images/omikuji_syoukichi.png',
+			'images/omikuji_kyou.png',
+	);
+	var random = Math.floor(Math.random() * img.length);
+	var result = '<img src =' +img[random]+ '>';
+	document.getElementById('result').innerHTML = result;
+}
+
