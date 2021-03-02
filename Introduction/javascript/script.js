62719dc959827c0f247b6fa717961f5b2211abde
nogami777 <78405789+nogami777@users.noreply.github.com>
2021-03-02 15:46:47 +0900

    Update script.js

--- Introduction/javascript/script.js
+++ Introduction/javascript/script.js
@@ -77,8 +77,7 @@ $(function() {
 
 $(function() {
 	$('.js-modal-open').on('click',function(){
-		var modalobj = $('.page');
-		var position = modalobj.offset().top; //ヘッダの高さ分位置をずらす
+		var position = $(this).offset().top; //ヘッダの高さ分位置をずらす
 		$('.js-modal').fadeIn();
 		return false;
 	});
