9ea9f2c0835499c75a3bfa0cabc4020639ee029f
nogami777 <78405789+nogami777@users.noreply.github.com>
2021-03-10 16:26:41 +0900

    Update script.js

--- Introduction/javascript/script.js
+++ Introduction/javascript/script.js
@@ -77,18 +77,16 @@ $(function() {
 
 
 //omikuji_modal
-$(function() {
-	$.when(
-		$('.js-modal-open2').on('click',function() {
-			   $('#omikuji_box').addClass('js-omikuji_box');
-//    });
-//	wait(2);
-	});
+//$(function() {
+//	$.when(
+//		$('.js-modal-open2').on('click',function() {
+//			   $('#omikuji_box').addClass('js-omikuji_box');
+//		});
 //).done($(function() {
-	).done(
-		$('#omikuji_box').addClass('js-omikuji_box');
-	);
-});
+//	).done(
+//		$('#omikuji_box').addClass('js-omikuji_box');
+//	);
+//});
 
 $(function() {
 	$('.js-modal-open').on('click',function(){
